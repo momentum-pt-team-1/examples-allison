bc8470adaa6ad2b8a67ed5aaa3105e30968be347
Rebecca Conley <rebecca.leigh.conley@gmail.com>
2021-04-20 10:50:51 -0400

    WIP add while loop for card_game

--- card_game.py
+++ card_game.py
@@ -1,3 +1,4 @@
+import random
 # Build a card game based on War
 
 # What I already know
@@ -11,26 +12,62 @@
 
 def make_deck():
     '''Create & return a deck of cards, 2 suits, 10 cards each'''
-    # for suits, use tuples, e.g. (1, ♥️)
+    # TODO for suits, use tuples, e.g. (1, ♥️)
     deck = [1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 6, 7, 7, 8, 8, 9, 9, 10, 10]
     return deck
 
-def shuffle_cards():
+def shuffle_cards(deck):
     '''Rearrange cards in random order and return deck'''
-    pass
+    random.shuffle(deck)
+    return deck
 
-def deal_cards():
+def deal_cards(deck):
     '''Deal the cards one at a time to each player until gone'''
-    pass
+    player1_hand = []
+    player2_hand = []
+    while len(deck) > 0:
+    # until the deck is empty    
+        player1_hand.append(deck.pop())
+        player2_hand.append(deck.pop())
+    return (player1_hand, player2_hand)
 
-def take_turn():
+def take_turn(player1_hand, player2_hand):
     '''players flip cards over, compare cards, player with larger card gets all cards
     If tie, flip another card and re-evaluate'''
-    pass
+    # both players flip one card on to table
+    table = []
+    round_over = False
+    while not round_over:
+        player1_card = player1_hand.pop()
+        player2_card = player2_hand.pop()
+        table.append(player1_card)
+        table.append(player2_card)
+        # if player1 card > player2 card: player 1 collects all cards
+        if player1_card > player2_card:
+            player1_hand.append(table)
+            round_over = True
+        # elif player2 card > player1 card: player2  collects all cards
+        elif player2_card > player1_card:
+            player2_hand.append(table)
+            round_over = True
+        # else: nothing
+        
+    
 
 def play_game():
     '''Execute the game process from creating deck to one player winning'''
+    # create a deck
     deck = make_deck()
+    # print out that deck
     print(deck)
+    # shuffle our deck
+    shuffled_deck = shuffle_cards(deck)
+    # show the shuffled deck
+    print(shuffled_deck)
+    # deal the cards
+    player1_hand, player2_hand = deal_cards(deck)
+    print("Player 1:", player1_hand)
+    print("Player 2:", player2_hand)
+    # repeat flip_and_compare until we run out of cards
 
 play_game()
