9e80bf9e7e2edf89b05872ba1888db540e2622bd
Rebecca Conley <rebecca.leigh.conley@gmail.com>
2021-04-29 10:31:12 -0400

    can calculate hand values

--- blackjack.py
+++ blackjack.py
@@ -38,6 +38,22 @@ class Player:
         else:
             return f"{self.name}"
 
+    def calculate_hand_value(self):
+        # TODO handle A having two potential values
+        total_value = 0
+        for card in self.hand:
+            if card.rank in ["J", "Q", "K"]:
+                total_value += 10
+            elif card.rank == "A":
+                total_value += 11
+            else:
+                total_value += card.rank
+        if total_value > 21:
+            if "A" in self.hand:
+                total_value -= 10
+        return total_value
+
+
 
 class Game:
     def __init__(self, player_name, dealer_name):
@@ -45,16 +61,29 @@ class Game:
         self.deck.shuffle()
         self.player = Player(player_name, False)
         self.dealer = Player(dealer_name, True)
-        self.player.hand.append(self.deck.cards.pop())
-        self.dealer.hand.append(self.deck.cards.pop())
-        self.player.hand.append(self.deck.cards.pop())
-        self.dealer.hand.append(self.deck.cards.pop())
+        for i in range(2):
+            self.deal_one_card(self.player)
+            self.deal_one_card(self.dealer)
+
+    def deal_one_card(self, person):
+        person.hand.append(self.deck.cards.pop())
+
+    def ask_hit_or_stay(player):
+        '''Determine whether player wants to hit or stay'''
+        # if player.is_dealer == True:
+        pass
+            
+       # return hit or stay
+     
 
 game = Game("Rusty Ryan", "Tracy")
 
 print(game.player)
 for card in game.player.hand:
     print(card)
+print(f'The total value of this hand is {game.player.calculate_hand_value()}')
 print(game.dealer)
 for card in game.dealer.hand:
     print(card)
+
+print(f'The total value of this hand is {game.dealer.calculate_hand_value()}')
