ba19e3ca97373ed9d075e831d67f6ed553d6bae4
Rebecca Conley <rebecca.leigh.conley@gmail.com>
2021-05-13 15:39:07 -0400

    basic homepage

--- todos/main/models.py
+++ todos/main/models.py
@@ -4,6 +4,8 @@ from django.db import models
 class Todo(models.Model):
     text = models.CharField(max_length=280)
     created_date = models.DateField(auto_now_add=True)
-    due_date = models.DateField()
-    done = models.BooleanField()
+    due_date = models.DateField(blank=True, null=True)
+    done = models.BooleanField(default=False)
 
+    def __str__(self):
+        return self.text
--- todos/main/views.py
+++ todos/main/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
+from .models import Todo
+
 # Create your views here.
+def homepage(request):
+    todos = Todo.objects.all()
+    return render(request, 'main/home.html', {'todos': todos})
